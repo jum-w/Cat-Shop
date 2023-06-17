cedc3e7ebc8c309d7e6dab00cffcce298963bd8d
jum <grayd647@gmail.com>
2023-06-17 17:49:08 +0100

    &apos; change

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center text-center mt-16">
         <Image src={cat} alt="picture of cat" className="mb-16 mx-4" height={192} />
         <h1 className="lg:w-1/3 text-5xl font-bold text-shadow mx-4">The <span className="text-blue1">Purrfect</span> Place for your Furry Friends.</h1>
-        <h2 className="mt-4 text-gray-600 text-xl mx-4">Your Cat's Happiness is Our Priority - Shop Our Collection.</h2>
+        <h2 className="mt-4 text-gray-600 text-xl mx-4">Your Cat&apos;s Happiness is Our Priority - Shop Our Collection.</h2>
         <Link href="/shop" className="mt-6 py-2 px-4 bg-blue1 hover:bg-blue2 duration-150 rounded-lg shadow-lg items-center flex text-gray-50 mb-8">Discover More <FontAwesomeIcon icon={faArrowRight} className="ml-3" /></Link>
       </div>
     </>
